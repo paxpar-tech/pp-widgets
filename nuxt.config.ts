a95c811b6c193704039da92d7ee3da243b7c33e3
Philippe ENTZMANN <philippe@entzmann.name>
2023-09-24 15:17:20 +0200

    amfix: wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
+import { createResolver, logger, defineNuxtModule } from '@nuxt/kit'
+import { $fetch } from 'ofetch'
+import { version } from './package.json'
+
+const { resolve } = createResolver(import.meta.url)
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: [
+    resolve('./assets/css/main.css')
+  ],
   devtools: { enabled: true },
   extends: [
     '@nuxt-themes/docus',
---
+++ nuxt.schema.ts
@@ -0,0 +1,17 @@
+export default defineNuxtSchema({
+  appConfig: {
+    /**
+     * pp-widgets configuration.
+     *
+     * @studioIcon material-symbols:docs
+     */
+    pp: {
+      /**
+       * Website title, used as header default title and meta title.
+       *
+       * @studioIcon material-symbols:title
+       */
+      title: 'pp-widgets',
+    }
+  }
+})
